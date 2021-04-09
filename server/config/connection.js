cf511b7f4a282baf066729e08e7784f437c5d1cf
Jennifer Kruk <jenkruk@gmail.com>
2021-04-09 14:42:13 -0700

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/sociaTea', {
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/sociaTea', 
+  {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
