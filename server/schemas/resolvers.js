cfb248c9ed7a8e645a18ed135dede1367e8ddfc1
Jennifer Kruk <jenkruk@gmail.com>
2021-04-09 15:07:40 -0700

    deploying

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -19,7 +19,7 @@ const resolvers = {
 
     // get all users
     users: async () => {
-        return User.find()
+        return await User.find()
         .select('-__v -password')
         .populate('friends')
         .populate('thoughts');
@@ -27,7 +27,7 @@ const resolvers = {
 
     // get a user by username
     user: async (parent, { username }) => {
-        return User.findOne({ username })
+        return await User.findOne({ username })
         .select('-__v -password')
         .populate('friends')
         .populate('thoughts');
@@ -35,11 +35,11 @@ const resolvers = {
 
     thoughts: async (parent, { username }) => {
         const params = username ? { username } : {};
-        return Thought.find(params).sort({ createdAt: -1 });
+        return await Thought.find(params).sort({ createdAt: -1 });
         },
 
     thought: async (parent, { _id }) => {
-    return Thought.findOne({ _id });
+    return await Thought.findOne({ _id });
     },
   },
 
